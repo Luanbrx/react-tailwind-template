b4d009666c5b37785d8f30ec42f2f58a38849178
luan menezesds <luanmenezesds615@gmail.com>
2026-03-04 18:08:01 -0300

    feat: setup inicial com vite, tailwind v4 e lucide

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className= "texto-3x1 font=bold">Hello Word React Tailwind</h1>
+    </>
+  );
+}
+
+export default App;
